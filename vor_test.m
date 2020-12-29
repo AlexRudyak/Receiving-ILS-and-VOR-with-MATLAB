e237ba45a79a3b9312936999e05e7cdbef7a34f9
Alex Rudyak <59541334+AlexRudyak@users.noreply.github.com>
2020-12-29 12:55:07 +0200

    Update vor_test.m

--- vor_test.m
+++ vor_test.m
@@ -3,25 +3,49 @@ ts = 1/fs;
 run_time = 0.03;
 t = 0:ts:run_time;
 
+%% AM (rotation)
 Aam = 0.33;
-phi = 3;
+phi = pi;
 xam = Aam*cos(2*pi*30*t - phi);
 
+%% Baseband signal 30 Hz
 Afm_bb = 0.33;
 xfm_bb = Afm_bb*cos(2*pi*30*t);
 
+%% FM 9960Hz with 30Hz
 Afm = 0.33;
 kfm = 0.33;
 xfm = Afm*cos(2*pi*9960*t + kfm.*(Afm_bb/60*pi*sin(2*pi*30*t)));
 
+%% Final VOR Signal equation 
 kam1 = 0.33;
 kam2 = 0.33;
 zvor = (1+kam1.*xam+kam2.*xfm).*cos(2*pi*108e6*t);
 
+
+% %% Receiver
+% raw_rx = zvor.*cos(2*pi*108e6*t);
+% rxlpf = angle(lowpass(raw_rx,10e3,300e6));
+% %rxlpf_bb = angle(rxlpf.*cos(2*pi*9960*t));
+%% Plots
+% Trasnmit
+figure(1);
 plot(t,zvor);
 hold on 
-plot(t,xam,'--g','LineWidth',2);
+plot(t,xam,'--g','LineWidth',2);    
 hold on
 plot(t,xfm_bb,'--r','LineWidth',2);
-legend('X_V_o_r','X_V_a_r','X_F_M_b_b');
+legend('X_{vor}','X_{variable}','X_{reference}');
+title('VOR trasmit');
 hold off
+
+% % Receive
+% figure(2);
+% %plot (t,raw_rx);
+% hold on
+% plot (t,rxlpf);
+% hold on
+% %plot (t, rxlpf_bb);
+% axis([0 run_time -2 4])
+% hold off
+
